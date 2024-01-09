00083de80b23949703f493422fd6a914cd711e53
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2024-01-09 22:14:50 +0400

    installed ants and bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import {Button} from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app p-5'>
+      <h1>Health</h1>
+      <Button type='primary'>Primary Button</Button>
     </div>
-  );
+  )
 }
 
 export default App;
