44a5163958f475421f7024ec18e66d3472d28bdf
manahil <manahil_siddiqui@hotmail.com>
2024-08-28 19:23:19 +0400

    updated users

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    mobileNumber: {
+        type: Number,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: [{
+        type: String,
+        default: 'Doctor'
+    }]
+})
+
+module.exports = mongoose.model('User', userSchema)
